3192d951702e88b946ecd57a065654b01a2f94d4
bhargavyagnik <41851612+bhargavyagnik@users.noreply.github.com>
2020-05-27 23:41:24 +0530

    code for random password generator

---
+++ main.py
@@ -0,0 +1,37 @@
+import random
+
+def generate_random_passwords(length=10):
+    if(length<7):
+        print("Not a good password")
+        length=10
+    DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    LOCASE_CHARACTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+                         'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q',
+                         'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+                         'z']
+
+    UPCASE_CHARACTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+                         'I', 'J', 'K', 'M', 'N', 'O', 'p', 'Q',
+                         'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+                         'Z']
+
+    SYMBOLS = ['@', '#', '$']
+    ALL=DIGITS+LOCASE_CHARACTERS+UPCASE_CHARACTERS+SYMBOLS
+    d=int(length*0.3)
+    l=int(length*0.3)
+    u=int(length*0.1)
+    s=length-(d+u+l)
+    digits=random.sample(population=DIGITS,k=d)
+    locase=random.sample(population=LOCASE_CHARACTERS,k=l)
+    upcase=random.sample(population=UPCASE_CHARACTERS,k=u)
+    symbols=random.sample(population=SYMBOLS,k=s)
+    all=digits+locase+upcase+symbols
+    random.shuffle(all)
+    string=''
+    for i in all:
+        string=string+str(i)
+    print(string)
+
+if(__name__=='__main__'):
+    generate_random_passwords()
+
